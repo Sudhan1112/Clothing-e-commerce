07c8b5af7fe20b21204f0fedba3e70985b62322a
sudhan1112 <sudhanssudhan83@gmail.com>
2025-03-26 12:04:13 +0530

    added cors

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,12 @@ connectCloudinary()
 
 // middlewares
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: "https://clothing-ec.netlify.app", 
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true 
+  }));
+  
 
 // Routes
 app.use('/api/user', userRouter)
